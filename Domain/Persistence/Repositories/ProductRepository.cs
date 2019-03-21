9a015714c2ec1453daf03816a2c437b6325040a9
ada saliy <a0676001993@gmail.com>
2019-03-21 15:56:47 +0200

    product repository

--- Domain/Persistence/Repositories/ProductRepository.cs
+++ Domain/Persistence/Repositories/ProductRepository.cs
@@ -15,7 +15,15 @@ namespace SupermarketApiRest.Domain.Persistence.Repositories
 
         public async Task<IEnumerable<Product>> ListAsync()
         {
-            return await _context.Products.ToListAsync();
+            /*Notice the call to Include(p => p.Category).
+             We can chain this syntax to include as many entities as necessary when querying data. 
+             EF Core is going to translate it to a join when performing the select.
+             */
+            return await _context.Products.Include(p => p.Category).ToListAsync();
+            /*EF Core, by default,
+             does not include related entities to your models when you querying data because
+             it could be very slow 
+             (imagine a model with ten related entities, all all related entities having its own relationships).*/
         }
 
         public async Task AddAsync(Product product) => await _context.Products.AddAsync(product);
